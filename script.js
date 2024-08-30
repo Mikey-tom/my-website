baf705d72bc43cc24f2e34df2429cd720da1e0fa
ANANDHA PRAKASH <110828827+Mikey-tom@users.noreply.github.com>
2024-08-30 17:32:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('Website is loaded and ready.');
+  console.log("Welcome to Anandhaprakash's World! Explore, learn, and enjoy.");
 });
+
